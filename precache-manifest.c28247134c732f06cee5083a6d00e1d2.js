e5e9b472306e7c7c4c200b6214926282ad48b38e
devevangel <dev.evangel@gmail.com>
2020-09-11 23:39:41 +0100

    Updates

---
+++ precache-manifest.c28247134c732f06cee5083a6d00e1d2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e789408894732e3323fc7b3d470da58",
+    "url": "/todo-v3/index.html"
+  },
+  {
+    "revision": "395995165145559fce31",
+    "url": "/todo-v3/static/js/2.7718d1b5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/todo-v3/static/js/2.7718d1b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8bedcd2852c77c20390d",
+    "url": "/todo-v3/static/js/main.7a268d82.chunk.js"
+  },
+  {
+    "revision": "e022e8a1f235483ee396",
+    "url": "/todo-v3/static/js/runtime-main.43740a27.js"
+  }
+]);
